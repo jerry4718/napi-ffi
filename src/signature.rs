7fabc151f74a4addf35b556e2346fa610a5e55b6
jerry4718 <jerry4718@qq.com>
2026-05-31 21:47:20 +0800

    -- remove type "Void"

--- src/signature.rs
+++ src/signature.rs
@@ -49,7 +49,7 @@ impl CompiledCallbackSignature {
 
 fn parse_target(type_name: &str) -> Result<Box<dyn TypedTarget>> {
   match type_name {
-    "void" | "Void" => Ok(Box::new(VoidTarget)),
+    "void" => Ok(Box::new(VoidTarget)),
     "char" => {
       if (std::ffi::c_char::MIN as i32) < 0 {
         Ok(Box::new(I8Target))
